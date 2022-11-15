92bca6b40b804e08273b07663bc5709583499d7a
visheshtgupta <vishesht.gupta@mutualmobile.com>
2022-11-15 14:31:07 +0530

    bypass the RN navigation

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -4,10 +4,7 @@ import { HomeScreen } from "./Home";
 import RootNavigator from "./navigation/root-navigator";
 
 export function App() {
-  return <HomeScreen />;
-  // return <RootNavigator />;
-
-  // for Nextjs use <HomeScreen />
+  return <RootNavigator />;
 }
 
 export default App;
